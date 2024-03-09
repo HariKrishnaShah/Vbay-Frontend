15e22f3c568abc53dc1e8f2b368c09d811136448
Hari <hksshah111@gmail.com>
2024-03-09 10:47:35 +0545

    Completed

---
+++ javascript/jsfile.js
@@ -0,0 +1,238 @@
+function showa()
+{
+	var x = document.getElementById("subitema");
+	var y = document.getElementById("iIcona");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showb()
+{
+	var x = document.getElementById("subitemb");
+	var y = document.getElementById("iIconb");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showc()
+{
+	var x = document.getElementById("subitemc");
+	var y = document.getElementById("iIconc");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showd()
+{
+	var x = document.getElementById("subitemd");
+	var y = document.getElementById("iIcond");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showe()
+{
+	var x = document.getElementById("subiteme");
+	var y = document.getElementById("iIcone");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showf()
+{
+	var x = document.getElementById("subitemf");
+	var y = document.getElementById("iIconf");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showg()
+{
+	var x = document.getElementById("subitemg");
+	var y = document.getElementById("iIcong");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showh()
+{
+	var x = document.getElementById("subitemh");
+	var y = document.getElementById("iIconh");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showi()
+{
+	var x = document.getElementById("subitemi");
+	var y = document.getElementById("iIconi");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showj()
+{
+	var x = document.getElementById("subitemj");
+	var y = document.getElementById("iIconj");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showk()
+{
+	var x = document.getElementById("subitemk");
+	var y = document.getElementById("iIconk");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showl()
+{
+	var x = document.getElementById("subiteml");
+	var y = document.getElementById("iIconl");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showm()
+{
+	var x = document.getElementById("subitemm");
+	var y = document.getElementById("iIconm");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function shown()
+{
+	var x = document.getElementById("subitemn");
+	var y = document.getElementById("iIconn");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+function showo()
+{
+	var x = document.getElementById("subitemo");
+	var y = document.getElementById("iIcono");
+	if(x.style.display === "block")
+	{
+		x.style.display = "none";
+		y.style.transform = "rotate(90deg)";
+	}
+	else
+	{
+		x.style.display = "block";
+		y.style.transform = "rotate(0deg)";
+	}
+}
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    items:1,
+    autoplay:true,
+    loop:true,
+    autoplayHoverPause:false,
+})
+});
+
+
